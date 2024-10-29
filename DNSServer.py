d2fee1a5f40e67d59fc40452ae441927eaf3ff89
Natalie Bruns <78214192+nab9478@users.noreply.github.com>
2024-10-29 09:53:52 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -86,7 +86,7 @@ dns_records = {
         dns.rdatatype.A: '192.168.1.106',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
-        dns.rdatatype.TXT: (encrypted_value, decrypted_value),
+        dns.rdatatype.TXT: (str(encrypted_value),),
         dns.rdatatype.NS: 'ns1.nyu.edu.'
         }
    
